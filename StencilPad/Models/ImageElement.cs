4478681583e40e714ec05e817246a1a78c809826
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-05-20 13:04:03 +0100

    ImageElement to use bounds handles

--- StencilPad/Models/ImageElement.cs
+++ StencilPad/Models/ImageElement.cs
@@ -4,19 +4,20 @@ namespace StencilPad.Models;
 
 public class ImageElement : SheetElement<ImageElement>
 {
-    public override MinMaxHandleSource HandleSource { get; }
+    public override BoundsHandleSource HandleSource { get; }
 
     public Unit2D Min
     {
-        get => HandleSource.Min;
-        set => HandleSource.Min = value;
+        get => HandleSource.Bounds.Min;
+        set => HandleSource.Bounds = UnitBounds.FromMinMax(value, HandleSource.Bounds.Max);
     }
 
     public Unit2D Max
     {
-        get => HandleSource.Max;
-        set => HandleSource.Max = value;
+        get => HandleSource.Bounds.Max;
+        set => HandleSource.Bounds = UnitBounds.FromMinMax(value, HandleSource.Bounds.Max);
     }
+
 
     private byte[] _imageData = [];
     public byte[] ImageData
@@ -33,48 +34,51 @@ public class ImageElement : SheetElement<ImageElement>
     
     public ImageElement()
     {
-        HandleSource = new MinMaxHandleSource(Unit2D.Zero, Unit2D.Zero);
+        HandleSource = new BoundsHandleSource(UnitBounds.Empty);
         HandleSource.HandleMoved += (_, _, _) => GeometryChanged?.Invoke();
     }
 
-    public ImageElement(Unit2D start, Unit2D end, byte[] imageData)
+    public ImageElement(Unit2D min, Unit2D max, byte[] imageData)
     {
-        HandleSource = new MinMaxHandleSource(start, end);
+        HandleSource = new BoundsHandleSource(UnitBounds.FromMinMax(min, max));
         HandleSource.HandleMoved += (_, _, _) => GeometryChanged?.Invoke();
-
         _imageData = imageData;
     }
 
     public override void MirrorX(Unit centerY)
     {
-        Min = new Unit2D(Min.X, (centerY * 2) - Min.Y);
-        Max = new Unit2D(Max.X, (centerY * 2) - Max.Y);
+        HandleSource.Bounds = UnitBounds.FromMinMax(
+            new Unit2D(Min.X, (centerY * 2) - Min.Y),
+            new Unit2D(Max.X, (centerY * 2) - Max.Y)
+        );
     }
 
     public override void MirrorY(Unit centerX)
     {
-        Min = new Unit2D((centerX * 2) - Min.X, Min.Y);
-        Max = new Unit2D((centerX * 2) - Max.X, Max.Y);
+        HandleSource.Bounds = UnitBounds.FromMinMax(
+            new Unit2D((centerX * 2) - Min.X, Min.Y),
+            new Unit2D((centerX * 2) - Max.X, Max.Y)
+        );
     }
 
     public override void Translate(Unit2D delta)
     {
-        HandleSource.Min += delta;
-        HandleSource.Max += delta;
+        HandleSource.Bounds += delta;
     }
 
     public override void AssignFrom(ImageElement other)
     {
-        Min = other.Min;
-        Max = other.Max;
+        HandleSource.AssignFrom(other.HandleSource);
         ImageData = other.ImageData;
     }
 
     public override ImageElement DeepClone()
     {
         var clone = new ImageElement();
+        
         clone.Id = Id;
         clone.AssignFrom(this);
+        
         return clone;
     }
 }
