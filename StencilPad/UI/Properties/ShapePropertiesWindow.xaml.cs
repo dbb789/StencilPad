0a8de097540c9c7c8a4aa3dc0481961a3c5e945e
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-05-25 09:34:32 +0100

    Cap dropdowns

--- StencilPad/UI/Properties/ShapePropertiesWindow.xaml.cs
+++ StencilPad/UI/Properties/ShapePropertiesWindow.xaml.cs
@@ -18,18 +18,73 @@ public partial class ShapePropertiesWindow : Window
         ViewModel = new ShapePropertiesViewModel(shapes);
         DataContext = ViewModel;
 
-        var capItems = new List<Geometry>()
+        var startCapItems = new List<Geometry>()
         {
-            resourceService.Get(GeometryResourceId.None),
-            resourceService.Get(GeometryResourceId.Arrow0)
+            CreateCapGeometry(resourceService, GeometryResourceId.None, true),
+            CreateCapGeometry(resourceService, GeometryResourceId.Arrow0, true)
         };
 
-        StartCapDropdown.Items = capItems;
-        EndCapDropdown.Items = capItems;
+        StartCapDropdown.Items = startCapItems;
+
+        var endCapItems = new List<Geometry>()
+        {
+            CreateCapGeometry(resourceService, GeometryResourceId.None, false),
+            CreateCapGeometry(resourceService, GeometryResourceId.Arrow0, false)
+        };
+
+        EndCapDropdown.Items = endCapItems;
     }
 
     private void Close_Click(object sender, RoutedEventArgs e)
     {
         Close();
+    }
+
+    private Geometry CreateCapGeometry(IResourceService resourceService,
+                                       GeometryResourceId resourceId,
+                                       bool startCap)
+    {
+        var cap = resourceService.Get(resourceId);
+        
+        var line = new StreamGeometry
+        {
+            FillRule = FillRule.EvenOdd
+        };
+
+        using (var ctx = line.Open())
+        {
+            ctx.BeginFigure(new Point(0, 0), true, false);
+            ctx.LineTo(new Point(0, 40), true, false);
+        }
+
+        line.Freeze();
+
+        var group = new GeometryGroup
+        {
+            FillRule = FillRule.EvenOdd
+        };
+        
+        group.Children.Add(cap);
+        group.Children.Add(line);
+
+        var transformGroup = new TransformGroup();
+        
+        if (startCap)
+        {
+            transformGroup.Children.Add(new TranslateTransform(-5, 0));
+            transformGroup.Children.Add(new RotateTransform(-90, 0, 0));
+        }
+        else
+        {
+            transformGroup.Children.Add(new TranslateTransform(5, -40));
+            transformGroup.Children.Add(new RotateTransform(90, 0, 0));
+        }
+
+        transformGroup.Freeze();
+        
+        group.Transform = transformGroup;
+        group.Freeze();
+        
+        return group;
     }
 }
