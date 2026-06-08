62553bcb136e128a7460a00f61f58ae199199e55
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-06-08 12:38:56 +0100

    Grid setup on toolbar

---
+++ StencilPad/UI/GridButtons.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StencilPad.UI;
+
+public partial class GridButtons : UserControl
+{
+    public static readonly DependencyProperty ShowGridProperty =
+        DependencyProperty.Register(nameof(ShowGrid), typeof(bool), typeof(GridButtons),
+            new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public static readonly DependencyProperty SnapToGridProperty =
+        DependencyProperty.Register(nameof(SnapToGrid), typeof(bool), typeof(GridButtons),
+            new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public static readonly DependencyProperty SnapToPointProperty =
+        DependencyProperty.Register(nameof(SnapToPoint), typeof(bool), typeof(GridButtons),
+            new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public bool ShowGrid
+    {
+        get => (bool)GetValue(ShowGridProperty);
+        set => SetValue(ShowGridProperty, value);
+    }
+
+    public bool SnapToGrid
+    {
+        get => (bool)GetValue(SnapToGridProperty);
+        set => SetValue(SnapToGridProperty, value);
+    }
+
+    public bool SnapToPoint
+    {
+        get => (bool)GetValue(SnapToPointProperty);
+        set => SetValue(SnapToPointProperty, value);
+    }
+    
+    public GridButtons()
+    {
+        InitializeComponent();
+    }
+}
