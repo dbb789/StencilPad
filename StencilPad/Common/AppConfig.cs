e26a1cc39c263d978d5114ab7c04fea6ac112795
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-06-09 11:15:55 +0100

    Colours

--- StencilPad/Common/AppConfig.cs
+++ StencilPad/Common/AppConfig.cs
@@ -5,11 +5,11 @@ namespace StencilPad.Common;
 
 public class AppConfig : IAppConfig
 {
-    public Color GridLineColor { get; set; } = Color.FromRgb(0, 128, 255);
+    public Color GridLineColor { get; set; } = Color.FromRgb(0, 127, 255);
     public Color SelectionColor { get; set; } = Color.FromRgb(0, 0, 255);
-    public Color GroupSelectionColor { get; set; } = Color.FromRgb(0, 128, 255);
-    public Color MoveHandleColor { get; set; } = Color.FromRgb(255, 128, 0);
-    public Color AdjustHandleColor { get; set; } = Color.FromRgb(0, 128, 0);
+    public Color GroupSelectionColor { get; set; } = Color.FromRgb(255, 0, 255);
+    public Color MoveHandleColor { get; set; } = Color.FromRgb(255, 127, 0);
+    public Color AdjustHandleColor { get; set; } = Color.FromRgb(0, 127, 0);
     
     public double HandleSizePx { get; set; } = 12.0;
     public double PointSnapPx { get; set; } = 32.0;
