88c38072d01910bc4248fabb1efb9b08883a6305
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-07-14 11:12:14 +0100

    Remove dead code

--- StencilPad/UI/MainWindow.xaml.cs
+++ StencilPad/UI/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 using System.Windows;
-using System.Windows.Media;
 
 namespace StencilPad.UI;
 
@@ -10,31 +9,5 @@ public partial class MainWindow : Window, IWpfDialogParent
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private static T? FindVisualChild<T>(DependencyObject? parent) where T : DependencyObject
-    {
-        if (parent == null)
-        {
-            return null;
-        }
-        
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); ++i)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-
-            if (child is T t)
-            {
-                return t;
-            }
-            
-            var result = FindVisualChild<T>(child);
-
-            if (result != null)
-            {
-                return result;
-            }
-        }
-        return null;
     }
 }
